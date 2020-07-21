921b1334271186126ff16781af8bda7083e32682
Yikai0121 <k2lairrol@gmail.com>
2020-07-21 23:36:35 +0800

    20200721

---
+++ 總複習練習/判斷是否質數且算出1-100質數總和/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace 判斷是否質數且算出1_100質數總和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = Primenumber(131);
+            System.Console.WriteLine(b);
+            int sum = PrimenumberSum();
+            System.Console.WriteLine(sum);
+        }
+        public static bool Primenumber(int n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        public static int PrimenumberSum()
+        {
+
+            int sum = 0;
+            for (int i = 2; i <= 100; i++)
+            {
+                bool b = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        b = false;
+                        break;
+                    }
+                }
+                if (b)
+                {
+                    sum += i;
+                }
+
+            }
+            return sum;
+        }
+    }
+}
---
+++ 總複習練習/計算1-100整數和和奇數和/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 計算1_100整數和和奇數和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumSum();
+            Oddnum();
+        }
+        public static void NumSum()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            System.Console.WriteLine(sum);
+        }
+        public static void Oddnum()
+        {   
+            int sum = 0;
+            for (int i = 1; i <=100; i+=2)
+            {   
+                sum +=i;
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
