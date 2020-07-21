2dd10183928508d5a1d2963f8a2570583f7865d3
Yikai0121 <k2lairrol@gmail.com>
2020-07-21 17:24:16 +0800

    20200721

--- 總複習練習/計算任意數個數最大值/Program.cs
+++ 總複習練習/計算任意數個數最大值/Program.cs
@@ -6,7 +6,8 @@ namespace 計算任意數個數最大值
     {
         static void Main(string[] args)
         {
-           System.Console.WriteLine(Max(1,8,5,4,2,3,3,6,5,2,1,1,2,100,2));
+            System.Console.WriteLine(Max(1, 8, 5, 4, 2, 3, 3, 6, 5, 2, 1, 1, 2, 100, 2));
+            System.Console.WriteLine(Max2(10, 50));
         }
 
         public static int Max(params int[] nums)
@@ -20,6 +21,11 @@ namespace 計算任意數個數最大值
                 }
             }
             return Max;
+        }
+        //比較兩數
+        public static int Max2(int n1, int n2)
+        {
+            return n1 > n2 ? n1 : n2;
         }
     }
 }
